a2141c11e65b6910a899ada249eb3a6c2af39378
sinan <sinan@test.tmp>
2012-08-01 09:11:26 +0200

    extracted engine creators to extra file

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,13 +1,7 @@
 #include "logger.h"
 #include "engine.h"
-
-#include "box_physic.h"
-#include "static_physic.h"
-
-#include "engine_gui_shape.h"
-#include "engine_spacecage.h"
-#include "engine_body.h"
-#include "engine_joint.h"
+#include "engine_physic.h"
+#include "engine_object.h"
 
 Engine::Engine() :
 	mLoopRendering(true),
@@ -133,71 +127,6 @@ void    Engine::addObject(EngineObject* object){
 int     Engine::howManyObjects() {
 	return mObjects.size();
 }
-
-EngineObject*	Engine::createMesh(const char* meshName){
-	EngineGuiShape* engineObject = new EngineGuiShape(this);
-	engineObject->createMesh(meshName);
-	engineObject->setSize(Vec3(10,1,10));
-	engineObject->setPosition(Vec3(0,-0.5,0));
-	return engineObject;
-}
-
-EngineObject*	Engine::createGuiBox(){
-	EngineGuiShape* engineObject = new EngineGuiShape(this);
-	engineObject->createBoxEntity();
-	engineObject->setSize(Vec3(10,1,10));
-	engineObject->setPosition(Vec3(0,-0.5,0));
-	return engineObject;
-}
-
-EngineObject*	Engine::createPhysicStatic(){
-	Logger::debug("phsyic static created");
-	EngineBody* engineObject = new EngineBody(this);
-	engineObject->setPhysicBody(
-		new PhysicStatic(
-			getPhysicsEngine(),
-			Vec3(0,0,0),
-			Vec3(1,1,1)
-			)
-	);
-	engineObject->setGuiUpdatesOff();
-	engineObject->createBoxEntity();
-	engineObject->setSize(Vec3(1,1,1));
-	engineObject->setPosition(Vec3(0,150,0));
-	return engineObject;
-}
-
-EngineObject*	Engine::createPhysicBox(){
-	EngineBody* engineObject = new EngineBody(this);
-	engineObject->setPhysicBody(
-		new PhysicBox(
-			getPhysicsEngine(),
-			Vec3(0,0,0),
-			Vec3(1,1,1)
-			)
-	);
-	engineObject->createBoxEntity();
-	engineObject->setSize(Vec3(1,1,1));
-	engineObject->setPosition(Vec3(0,150,0));
-	return engineObject;
-}
-
-EngineObject*	Engine::createJoint(EngineObject* obj1,EngineObject* obj2){
-	if (obj1->isBody() && obj2->isBody() ) {
-		EngineJoint* engineObject = new EngineJoint(this,obj1->isBody(),obj2->isBody());
-		return engineObject;
-	}
-	return 0;
-}
-
-EngineObject*	Engine::createSpaceCage(Vec3& size){
-	EngineSpaceCage* spaceCage = new EngineSpaceCage(this,size);
-	spaceCage->createBoxEntity();
-	spaceCage->setSize(Vec3(size.X(),1,size.Z()));
-	spaceCage->setPosition(Vec3(0,-0.5,0));
-	return spaceCage;
-}
-
 
 
 
---
+++ engine/engine_creators.cpp
@@ -0,0 +1,79 @@
+#include "logger.h"
+#include "engine.h"
+
+#include "box_physic.h"
+#include "static_physic.h"
+
+#include "engine_gui_shape.h"
+#include "engine_spacecage.h"
+#include "engine_body.h"
+#include "engine_joint.h"
+#include "engine_object.h"
+#include "engine_mesh.h"
+
+EngineObject*	Engine::createMesh(const char* meshName){
+	EngineGuiShape* engineObject = new EngineGuiShape(this);
+	engineObject->createMesh(meshName);
+	engineObject->setSize(Vec3(10,1,10));
+	engineObject->setPosition(Vec3(0,-0.5,0));
+	return engineObject;
+}
+
+EngineObject*	Engine::createGuiBox(){
+	EngineGuiShape* engineObject = new EngineGuiShape(this);
+	engineObject->createBoxEntity();
+	engineObject->setSize(Vec3(10,1,10));
+	engineObject->setPosition(Vec3(0,-0.5,0));
+	return engineObject;
+}
+
+EngineObject*	Engine::createPhysicStatic(){
+	Logger::debug("phsyic static created");
+	EngineBody* engineObject = new EngineBody(this);
+	engineObject->setPhysicBody(
+		new PhysicStatic(
+			getPhysicsEngine(),
+			Vec3(0,0,0),
+			Vec3(1,1,1)
+			)
+	);
+	engineObject->setGuiUpdatesOff();
+	engineObject->createBoxEntity();
+	engineObject->setSize(Vec3(1,1,1));
+	engineObject->setPosition(Vec3(0,150,0));
+	return engineObject;
+}
+
+EngineObject*	Engine::createPhysicBox(){
+	EngineBody* engineObject = new EngineBody(this);
+	engineObject->setPhysicBody(
+		new PhysicBox(
+			getPhysicsEngine(),
+			Vec3(0,0,0),
+			Vec3(1,1,1)
+			)
+	);
+	engineObject->createBoxEntity();
+	engineObject->setSize(Vec3(1,1,1));
+	engineObject->setPosition(Vec3(0,150,0));
+	return engineObject;
+}
+
+EngineObject*	Engine::createJoint(EngineObject* obj1,EngineObject* obj2){
+	if (obj1->isBody() && obj2->isBody() ) {
+		EngineJoint* engineObject = new EngineJoint(this,obj1->isBody(),obj2->isBody());
+		return engineObject;
+	}
+	return 0;
+}
+
+EngineObject*	Engine::createSpaceCage(Vec3& size){
+	EngineSpaceCage* spaceCage = new EngineSpaceCage(this,size);
+	spaceCage->createBoxEntity();
+	spaceCage->setSize(Vec3(size.X(),1,size.Z()));
+	spaceCage->setPosition(Vec3(0,-0.5,0));
+	return spaceCage;
+}
+
+
+
